3e5341ee5c788fcd03f7ff8e60b75813bd6b8d53
uneronnik <NIKI.FOMENKO.7@GMAIL.COM>
2021-09-08 00:10:12 +0300

    1.0

---
+++ UniversityHumanClasses/Classes/Applicant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityHumanClasses.Classes
+{
+    class Applicant : Human
+    {
+        private int _markNumber;
+
+        public int MarkNumber { get => _markNumber; }
+
+        public Applicant(DateTime birthDay, string name, int markNumber)
+            : base(birthDay, name)
+        {
+            _markNumber = markNumber;
+        }
+        public Applicant(Human human, int markNumber)
+            : base(human.BirthDay, human.Name)
+        {
+            _markNumber = markNumber;
+        }
+    }
+}
---
+++ UniversityHumanClasses/Classes/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityHumanClasses.Classes
+{
+    class Human
+    {
+        private DateTime _birthDay;
+        private string _name;
+
+        public DateTime BirthDay { get => _birthDay; }
+        public string Name { get => _name; }
+
+        public Human(DateTime birthDay, string name)
+        {
+            _birthDay = birthDay;
+            _name = name;
+        }
+    }
+}
